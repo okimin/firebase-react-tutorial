cb0d09cc7e72c815a036a13a0fa4f66b8a01f060
Daniel Gaston <danielgaston6@gmail.com>
2019-09-24 18:52:50 -0400

    First intitaialized git

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title"> Project Title</span>
+          <p>
+            Hiya Im a Petal Bird. But a bird that is caged. But one day he will
+            be free
+          </p>
+        </div>
+        <div className="card-action gret lighten-4 grey-text">
+          <div>Posted by the Net Ninja</div>
+          <div>2nd September , 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
